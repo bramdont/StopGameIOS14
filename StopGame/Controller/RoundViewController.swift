f45254e4e0b784334eef392708f444ec7c64ed5d
Bramdont Garcia <bramdont@gmail.com>
2021-03-30 17:38:54 -0400

    Initial Commit

---
+++ StopGame/Controller/RoundViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RoundViewController.swift
+//  StropGame
+//
+//  Created by Bramdont José García Aponte on 30/3/21.
+//
+
+import UIKit
+
+class RoundViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ StopGame/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StropGame
+//
+//  Created by Bramdont José García Aponte on 30/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
